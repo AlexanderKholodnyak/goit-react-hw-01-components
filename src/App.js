b5daed8d0ce324ad65c096f3e7c20bd1bbb886aa
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2021-01-08 23:52:49 +0200

    first task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Profile from './components/profiles/Profile';
+import user from './components/profiles/user.json';
+
 function App() {
   return (
     <div>
       <h1>Social-profile</h1>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
---
+++ src/components/profiles/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = ({ name, tag, location, avatar, stats }) => (
+  <div>
+    <div>
+      <img src={avatar} alt="Аватар пользователя" />
+      <p>{name}</p>
+      <p>{tag}</p>
+      <p>{location}</p>
+    </div>
+
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{stats.followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{stats.views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Profile;
